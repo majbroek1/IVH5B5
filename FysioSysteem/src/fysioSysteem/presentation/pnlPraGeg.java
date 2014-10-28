e3c396f53a164866abb8a8a84c96bb2ceb88d9b6
Pascal <Pascal@pascals-mbp.dynamic.ziggo.nl>
2014-10-28 14:15:48 +0100

    Panel Praktijk Gegevens Klaar

--- FysioSysteem/src/fysioSysteem/presentation/pnlPraGeg.java
+++ FysioSysteem/src/fysioSysteem/presentation/pnlPraGeg.java
@@ -1,12 +1,21 @@
 package fysioSysteem.presentation;
 
+import javax.swing.BorderFactory;
+import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
+
+import java.awt.Color;
 import java.awt.Label;
+
 import javax.swing.JButton;
+import javax.swing.border.Border;
+
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import java.util.ArrayList;
 
 import fysioSysteem.businessLogic.beheer.*;
 import fysioSysteem.domain.Praktijk;
@@ -19,13 +28,15 @@ public class pnlPraGeg extends JPanel{
 	private JTextField txtPraGegTelnr;
 	private JTextField txtPraGegEmail;
 	private JTextField txtPraGegWebsite;
+	private JTextField txtPraGegPostcode;
+	private int praktijkID = 1;
 	
 	public pnlPraGeg(){
 		setLayout(null);		
 		
 		IPraktijkManager prakManager = new PraktijkManager();
 		
-		Praktijk p = prakManager.getPraktijk(1);
+		Praktijk p = prakManager.getPraktijk(praktijkID);
 		
 		/* Labels */
 		JLabel lblPraGegTitel = new JLabel("Praktijk Gegevens");
@@ -40,24 +51,28 @@ public class pnlPraGeg extends JPanel{
 		lblPraGegNaam.setBounds(74, 57, 98, 16);
 		add(lblPraGegNaam);
 		
+		JLabel lblPraktijkPostcode = new JLabel("Praktijk Postcode");
+		lblPraktijkPostcode.setBounds(74, 118, 120, 16);
+		add(lblPraktijkPostcode);
+		
 		JLabel lblPraktijkPlaats = new JLabel("Praktijk Plaats");
-		lblPraktijkPlaats.setBounds(74, 118, 98, 16);
+		lblPraktijkPlaats.setBounds(438, 118, 98, 16);
 		add(lblPraktijkPlaats);
 		
 		JLabel lblPraktijkIban = new JLabel("Praktijk IBAN");
-		lblPraktijkIban.setBounds(438, 118, 98, 16);
+		lblPraktijkIban.setBounds(74, 177, 98, 16);
 		add(lblPraktijkIban);
 		
 		JLabel lblPraktijkTellefoonnummer = new JLabel("Praktijk Telefoonnummer");
-		lblPraktijkTellefoonnummer.setBounds(74, 177, 172, 16);
+		lblPraktijkTellefoonnummer.setBounds(438, 177, 172, 16);
 		add(lblPraktijkTellefoonnummer);
 		
 		JLabel lblPraktijkEmail = new JLabel("Praktijk Email");
-		lblPraktijkEmail.setBounds(438, 177, 172, 16);
+		lblPraktijkEmail.setBounds(74, 231, 172, 16);
 		add(lblPraktijkEmail);
 		
 		JLabel lblPraktijkWebsite = new JLabel("Praktijk Website");
-		lblPraktijkWebsite.setBounds(74, 237, 172, 16);
+		lblPraktijkWebsite.setBounds(438, 231, 172, 16);
 		add(lblPraktijkWebsite);
 		
 		/* TextFields */
@@ -69,37 +84,43 @@ public class pnlPraGeg extends JPanel{
 		
 		txtPraGegAdres = new JTextField();
 		txtPraGegAdres.setColumns(10);
-		txtPraGegAdres.setBounds(568, 51, 134, 28);
+		txtPraGegAdres.setBounds(605, 51, 134, 28);
 		txtPraGegAdres.setText(p.getAdres());
 		add(txtPraGegAdres);
 		
+		txtPraGegPostcode = new JTextField();
+		txtPraGegPostcode.setText(p.getPostcode());
+		txtPraGegPostcode.setColumns(10);
+		txtPraGegPostcode.setBounds(259, 112, 134, 28);
+		add(txtPraGegPostcode);
+		
 		txtPraGegPlaats = new JTextField();
 		txtPraGegPlaats.setColumns(10);
-		txtPraGegPlaats.setBounds(259, 112, 134, 28);
+		txtPraGegPlaats.setBounds(605, 112, 134, 28);
 		txtPraGegPlaats.setText(p.getPlaats());
 		add(txtPraGegPlaats);
 		
 		txtPraGegIBAN = new JTextField();
 		txtPraGegIBAN.setColumns(10);
-		txtPraGegIBAN.setBounds(568, 112, 134, 28);
+		txtPraGegIBAN.setBounds(259, 171, 134, 28);
 		txtPraGegIBAN.setText(p.getIban());
 		add(txtPraGegIBAN);
 		
 		txtPraGegTelnr = new JTextField();
 		txtPraGegTelnr.setColumns(10);
-		txtPraGegTelnr.setBounds(259, 171, 134, 28);
+		txtPraGegTelnr.setBounds(605, 171, 134, 28);
 		txtPraGegTelnr.setText(p.getTelNr());
 		add(txtPraGegTelnr);
 		
 		txtPraGegEmail = new JTextField();
 		txtPraGegEmail.setColumns(10);
-		txtPraGegEmail.setBounds(568, 171, 134, 28);
+		txtPraGegEmail.setBounds(258, 225, 134, 28);
 		txtPraGegEmail.setText(p.getEmail());
 		add(txtPraGegEmail);
 		
 		txtPraGegWebsite = new JTextField();
 		txtPraGegWebsite.setColumns(10);
-		txtPraGegWebsite.setBounds(259, 231, 134, 28);
+		txtPraGegWebsite.setBounds(605, 225, 134, 28);
 		txtPraGegWebsite.setText(p.getWebsite());
 		add(txtPraGegWebsite);
 		
@@ -111,8 +132,92 @@ public class pnlPraGeg extends JPanel{
 		/* Button Handling */
 		btnPraGegOpslaan.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
+				JFrame frame = new JFrame();
+				ArrayList<String> errorMessages = new ArrayList<String>();
+				Border redBorder = BorderFactory.createLineBorder(Color.red);
 				
+				if (txtPraGegNaam.getText().equals("")|| txtPraGegNaam.getText().length() >= 30)
+				{
+					txtPraGegNaam.setBorder(redBorder);
+					errorMessages.add("Voer de praktijk naam correct in!");
+				}
+				if (txtPraGegAdres.getText().equals("")|| txtPraGegAdres.getText().length() >= 50)
+				{
+					txtPraGegAdres.setBorder(redBorder);
+					errorMessages.add("Voer het praktijk adres correct in!");
+				}
+				if (txtPraGegPostcode.getText().equals("")|| !validatePostcode(txtPraGegPostcode.getText()))
+				{
+					txtPraGegPostcode.setBorder(redBorder);
+					errorMessages.add("Voer de praktijk postcode correct in!");
+				}
+				if (txtPraGegPlaats.getText().equals("")|| txtPraGegPlaats.getText().length() >= 50)
+				{
+					txtPraGegPlaats.setBorder(redBorder);
+					errorMessages.add("Voer het praktijk plaatsnaam correct in!");
+				}
+				if (txtPraGegIBAN.getText().equals("")|| txtPraGegIBAN.getText().length() >= 35)
+				{
+					txtPraGegIBAN.setBorder(redBorder);
+					errorMessages.add("Voer het praktijk IBAN nummer correct in!");
+				}
+				if (txtPraGegTelnr.getText().equals("")|| txtPraGegTelnr.getText().length() >= 30)
+				{
+					txtPraGegTelnr.setBorder(redBorder);
+					errorMessages.add("Voer het praktijk telefoonnummer correct in!");
+				}
+				if (txtPraGegEmail.getText().equals("")|| txtPraGegEmail.getText().length() >= 50 || !validateEmail(txtPraGegEmail.getText()))
+				{
+					txtPraGegEmail.setBorder(redBorder);
+					errorMessages.add("Voer het praktijk email adres correct in!");
+				}
+				if (txtPraGegWebsite.getText().equals("")|| txtPraGegWebsite.getText().length() >= 50)
+				{
+					txtPraGegWebsite.setBorder(redBorder);
+					errorMessages.add("Voer het praktijk website adres correct in!");
+				}
+				if (errorMessages.size() == 0)
+				{
+					Praktijk praktijk = new Praktijk(praktijkID, txtPraGegNaam.getText(), txtPraGegAdres.getText(), 
+							txtPraGegPostcode.getText(), txtPraGegPlaats.getText(), txtPraGegIBAN.getText(), 
+							txtPraGegTelnr.getText(), txtPraGegEmail.getText(), txtPraGegWebsite.getText());
+				    prakManager.setPraktijk(praktijk);
+				    
+				    revalidate();
+				    repaint();
+				    
+				    JOptionPane.showMessageDialog(frame, "De praktijk gegevens zijn succesvol opgeslagen.");
+				}
 			}
 		});
+	}
+	
+	private boolean validatePostcode(String postcode)
+	{
+		String postcodeRegex = "^[1-9]{1}[0-9]{3} ?[A-Z]{2}$";
+		
+		boolean check = false;
+		
+		if(postcode.matches(postcodeRegex))
+		{
+			check = true;
+		}
+		
+		return check;
+	}
+	
+	private boolean validateEmail(String email)
+	{
+		String emailRegex = "^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@"
+				+ "[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$";
+		
+		boolean check = false;
+		
+		if(email.matches(emailRegex))
+		{
+			check = true;
+		}
+		
+		return check;
 	}
 }
