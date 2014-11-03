f27fa6616122b7e7d672f22c814fb5db3bbaa314
fazed <bobhorvers@gmail.com>
2014-11-03 10:04:33 +0100

    RoosterManagerTest toegevoegd

--- FysioSysteem/src/fysioSysteem/tests/RoosterManagerTest.java
+++ FysioSysteem/src/fysioSysteem/tests/RoosterManagerTest.java
@@ -5,7 +5,6 @@ package fysioSysteem.tests;
 
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Calendar;
 import java.util.Date;
 
@@ -18,9 +17,7 @@ import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
 
 import fysioSysteem.businessLogic.planning.RoosterManager;
-import fysioSysteem.dataStorage.AfspraakDAO;
 import fysioSysteem.dataStorage.RoosterDAO;
-import fysioSysteem.domain.Afspraak;
 import fysioSysteem.domain.Fysiotherapeut;
 import fysioSysteem.domain.Praktijk;
 import fysioSysteem.domain.Rooster;
@@ -116,7 +113,6 @@ public class RoosterManagerTest {
 	public void testAddRoosterFailEndBeforeStart() {
 		PowerMockito.mockStatic(RoosterDAO.class);
 		
-		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss");
 		Praktijk p = new Praktijk(1, "Test", "test", "2991AL", "Breda", "1234567891", "leeg", "leeg", "leeg");
 		Fysiotherapeut f = new Fysiotherapeut(1, "Bob", "test", Status.ACTIEF, p);
 		
@@ -139,50 +135,6 @@ public class RoosterManagerTest {
 		boolean pass = rManager.addRooster(new Rooster(1, start, end, f));
 		
 		Assert.assertFalse(pass);
-	}
-	
-	@Test
-	public void testGetWeekRoosterPass() {
-		PowerMockito.mockStatic(RoosterDAO.class);
-		
-		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss");
-		
-		Date start = null;
-		Date end = null;
-		
-		try {
-			start = new Date(sdf.parse("2012-10-28T15:00:00").getTime());
-			end = new Date(sdf.parse("2012-10-28T16:00:00").getTime());
-		}
-		catch(Exception ex) {
-			// TODO
-		}
-		
-		Praktijk p = new Praktijk(1, "Test", "test", "2991AL", "Breda", "1234567891", "leeg", "leeg", "leeg");
-		Fysiotherapeut f = new Fysiotherapeut(1, "Bob", "test", Status.ACTIEF, p);
-		Rooster r = new Rooster(1, start, end, f);
-		
-		Mockito.when(RoosterDAO.getRooster(f, 45)).thenReturn(r);
-		
-		RoosterManager rManager = new RoosterManager();
-		Rooster rooster = rManager.getWeekRooster(f, 45);
-		
-		Assert.assertSame(r, rooster);
-	}
-	
-	@Test
-	public void testGetWeekRoosterFail() {
-		PowerMockito.mockStatic(RoosterDAO.class);
-		
-		Praktijk p = new Praktijk(1, "Test", "test", "2991AL", "Breda", "1234567891", "leeg", "leeg", "leeg");
-		Fysiotherapeut f = new Fysiotherapeut(1, "Bob", "test", Status.ACTIEF, p);
-		
-		Mockito.when(RoosterDAO.getRooster(f, 45)).thenReturn(null);
-		
-		RoosterManager rManager = new RoosterManager();
-		Rooster r = rManager.getWeekRooster(f, 45);
-		
-		Assert.assertNull(r);
 	}
 
 }
