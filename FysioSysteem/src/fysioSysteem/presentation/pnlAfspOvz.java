39d6f839ab2b3d2e3069617363560700a956da25
JorickDJ <jorick_de_j@hotmail.com>
2014-11-03 16:49:54 +0100

    Behandelingoverzicht tabel toegevoegd

--- FysioSysteem/src/fysioSysteem/presentation/pnlAfspOvz.java
+++ FysioSysteem/src/fysioSysteem/presentation/pnlAfspOvz.java
@@ -1,35 +1,72 @@
 package fysioSysteem.presentation;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
+
+import fysioSysteem.businessLogic.planning.IAfspraakManager;
+import fysioSysteem.dataStorage.AfspraakDAO;
+import fysioSysteem.domain.Afspraak;
+import fysioSysteem.domain.Fysiotherapeut;
 
 
 public class pnlAfspOvz extends JPanel {
 	
+	private JTable tblAfspOvzTable;
+	private ArrayList<Afspraak> afspraken;
+	
 	public pnlAfspOvz(){
-		//IAfspraakManager afsp = AppInjector IAfspraakManager();
-		//ILoginManager lgn = new ILoginManager();
-		//IMedewerkerManager mdwrk = new IMedewerkerManager();
+		afspraken = AfspraakDAO.getAfspraken();
 		
 		setLayout(null);
 		
-		/*
-		String[] tabelKolomNamen = {"Afspraak #", "Patient", "Datum" , "Tijd", "Behandeling"};
-		Object[][] data = { {new Integer(1), "Smith","23-10-2014", "14:12", "Knieblessure"}};*/
-		
-		/*
-		JTable tblAfspOvzTable = new JTable();
-		tblAfspOvzTable.setBounds(199, 19, 0, 0);
-		add(tblAfspOvzTable);
-		*/
-		
 		renderControls();
+		setTable();
 	}
 	
 	private void renderControls() {
-		JLabel lblAfspOvzTitel = new JLabel("Afspraak Overzicht");
-		lblAfspOvzTitel.setBounds(74, 11, 120, 16);
-		add(lblAfspOvzTitel);
+		tblAfspOvzTable = new JTable();
+		JScrollPane scrollPane = new JScrollPane(tblAfspOvzTable);
+		scrollPane.setBounds(10, 10, 867, 590);
+		add(scrollPane);
+	}
+	
+	private void setTable() {
+		DefaultTableModel mdl = new DefaultTableModel(
+			new Object[]{"Afspraak #", "Patient", "Datum" , "Tijd", "Behandeling"}, 0){
+		
+			// Tabel Bewerken uit
+			@Override
+		    public boolean isCellEditable(int row, int column) {
+		       return false;
+		    }
+		};
+		
+		SimpleDateFormat dateFormat = new SimpleDateFormat("EEE, d MMM");
+		SimpleDateFormat timeFormat = new SimpleDateFormat("HH:mm");
+		Date today = new Date();
+		
+		for(Afspraak a : afspraken) {
+			if(dateFormat.format(a.getDatumTijd()).equals(dateFormat.format(today)) && today.before(a.getDatumTijd())) {
+				mdl.addRow(new Object[]{
+					a.getId(), 
+					a.getBehandeling().getKlant().getNaam(),
+					dateFormat.format(a.getDatumTijd()), 
+					timeFormat.format(a.getDatumTijd()),
+					a.getBehandeling().getBehandelCode().getBehandelingNaam()
+				});
+			}
+		}
+		
+		tblAfspOvzTable.setModel(mdl);
 	}
 	
 }
