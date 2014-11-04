1ca6c4ff1273500611cebaa5e8e4d41be9d6194b
fazed <bobhorvers@gmail.com>
2014-11-04 21:12:56 +0100

    Merge origin/master
    Conflicts:
    	FysioSysteem/.gitignore

--- FysioSysteem/src/fysioSysteem/presentation/HoofdVenster.java
+++ FysioSysteem/src/fysioSysteem/presentation/HoofdVenster.java
@@ -56,6 +56,8 @@ public class HoofdVenster extends javax.swing.JFrame {
         jLabel5 = new javax.swing.JLabel();
         jPanel1 = new javax.swing.JPanel();
         jLabel6 = new javax.swing.JLabel();
+        btnBehandeCodes = new javax.swing.JPanel();
+        jLabel7 = new javax.swing.JLabel();
         pnlContent = new javax.swing.JPanel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
@@ -179,7 +181,7 @@ public class HoofdVenster extends javax.swing.JFrame {
                 .addContainerGap())
         );
 
-        btnUitloggen.setBackground(new java.awt.Color(0, 137, 137));
+        btnUitloggen.setBackground(new java.awt.Color(0, 102, 102));
         btnUitloggen.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
         btnUitloggen.addMouseListener(new java.awt.event.MouseAdapter() {
             public void mouseClicked(java.awt.event.MouseEvent evt) {
@@ -209,6 +211,7 @@ public class HoofdVenster extends javax.swing.JFrame {
         );
 
         jPanel1.setBackground(new java.awt.Color(0, 137, 137));
+        jPanel1.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
         jPanel1.addMouseListener(new java.awt.event.MouseAdapter() {
             public void mouseClicked(java.awt.event.MouseEvent evt) {
                 jPanel1MouseClicked(evt);
@@ -236,6 +239,35 @@ public class HoofdVenster extends javax.swing.JFrame {
                 .addContainerGap())
         );
 
+        btnBehandeCodes.setBackground(new java.awt.Color(0, 137, 137));
+        btnBehandeCodes.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
+        btnBehandeCodes.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                btnBehandeCodesMouseClicked(evt);
+            }
+        });
+
+        jLabel7.setFont(new java.awt.Font("Tahoma", 1, 12)); // NOI18N
+        jLabel7.setForeground(new java.awt.Color(255, 255, 255));
+        jLabel7.setText("Behandelcodes");
+
+        javax.swing.GroupLayout btnBehandeCodesLayout = new javax.swing.GroupLayout(btnBehandeCodes);
+        btnBehandeCodes.setLayout(btnBehandeCodesLayout);
+        btnBehandeCodesLayout.setHorizontalGroup(
+            btnBehandeCodesLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(btnBehandeCodesLayout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(jLabel7, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addContainerGap())
+        );
+        btnBehandeCodesLayout.setVerticalGroup(
+            btnBehandeCodesLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(btnBehandeCodesLayout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(jLabel7, javax.swing.GroupLayout.DEFAULT_SIZE, 18, Short.MAX_VALUE)
+                .addContainerGap())
+        );
+
         javax.swing.GroupLayout pnlMenuLayout = new javax.swing.GroupLayout(pnlMenu);
         pnlMenu.setLayout(pnlMenuLayout);
         pnlMenuLayout.setHorizontalGroup(
@@ -248,7 +280,8 @@ public class HoofdVenster extends javax.swing.JFrame {
                     .addComponent(btnBehandelingen, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                     .addComponent(btnPraktijk, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                     .addComponent(btnUitloggen, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                    .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                    .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(btnBehandeCodes, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
                 .addContainerGap())
         );
         pnlMenuLayout.setVerticalGroup(
@@ -263,8 +296,10 @@ public class HoofdVenster extends javax.swing.JFrame {
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(btnBehandeCodes, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(btnPraktijk, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 251, Short.MAX_VALUE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 205, Short.MAX_VALUE)
                 .addComponent(btnUitloggen, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addContainerGap())
         );
@@ -327,8 +362,13 @@ public class HoofdVenster extends javax.swing.JFrame {
         setPanel(new KlantOverzichtPanel());
     }//GEN-LAST:event_jPanel1MouseClicked
 
+    private void btnBehandeCodesMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_btnBehandeCodesMouseClicked
+        setPanel(new BehandelCodeOverzichtPanel());
+    }//GEN-LAST:event_btnBehandeCodesMouseClicked
+
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JPanel btnAfspraken;
+    private javax.swing.JPanel btnBehandeCodes;
     private javax.swing.JPanel btnBehandelingen;
     private javax.swing.JPanel btnPraktijk;
     private javax.swing.JPanel btnTherapeuten;
@@ -339,6 +379,7 @@ public class HoofdVenster extends javax.swing.JFrame {
     private javax.swing.JLabel jLabel4;
     private javax.swing.JLabel jLabel5;
     private javax.swing.JLabel jLabel6;
+    private javax.swing.JLabel jLabel7;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JPanel pnlContent;
     private javax.swing.JPanel pnlMenu;
