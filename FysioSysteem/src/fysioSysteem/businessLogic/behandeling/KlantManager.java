1b7f4f7f4604c05f94de83a5b03e2b6e5e73dc47
Mark <mbartele@student.avans.nl>
2014-11-04 23:15:55 +0100

    KlantManager

--- FysioSysteem/src/fysioSysteem/businessLogic/behandeling/KlantManager.java
+++ FysioSysteem/src/fysioSysteem/businessLogic/behandeling/KlantManager.java
@@ -16,18 +16,27 @@ public class KlantManager implements IKlantManager {
 
     @Override
     public void addKlant(Klant klant) {
-        // TODO Auto-generated method stub
-
+// TODO Auto-generated method stub
     }
 
     @Override
     public void setKlant(Klant klant) {
-        // TODO Auto-generated method stub
+// TODO Auto-generated method stub
+    }
 
-	@Override
-	public ArrayList<Klant> getKlanten() {
-		// TODO Auto-generated method stub
-		return KlantDAO.getKlanten();
-	}
+    @Override
+    public Klant getKlant(int id) {
+// TODO Auto-generated method stub
+        return null;
+    }
 
+    @Override
+    public Klant getKlant(String bsn) {
+        return KlantDAO.getKlant(bsn);
+    }
+
+    @Override
+    public ArrayList<Klant> getKlanten() {
+        return KlantDAO.getKlanten();
+    }
 }
